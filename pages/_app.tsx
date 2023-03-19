4a463789ed42339b7ed85a0abebaad515d7dab1c
josubin47 <3_3_jimca@naver.com>
2023-03-20 00:10:15 +0900

    feat: 앱 구조 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import Footer from '@/src/components/Footer';
+import Header from '@/src/components/Header';
+import type { AppProps } from 'next/app';
+import { Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const currentPath = router.asPath;
+
+  return (
+    <ChakraProvider>
+      <Header currentPath={currentPath} />
+      <Flex as="main">
+        <Flex
+          as="section"
+          direction="column"
+          width="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Component {...pageProps} />
+        </Flex>
+      </Flex>
+      <Footer />
+    </ChakraProvider>
+  );
 }
